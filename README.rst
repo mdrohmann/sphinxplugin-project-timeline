102841a150adf8ff1675d85e547cc70689cd23d7
Martin Drohmann <mdrohmann@gmail.com>
2015-03-05 07:39:57 -0800

    updated module setup

---
+++ README.rst
@@ -0,0 +1,10 @@
+Project timeline planner plugin for sphinx
+==========================================
+
+This plugin creates a project timeline out of a Sphinx_ document consisting of
+several sections of project tasks.  It computes the requested time for this
+project and defined milestones or deadlines.  This allows the user to stay on
+top of the development tasks, and it is requires far less maintenance than
+other project management tools.
+
+.. _Sphinx: http://sphinx-doc.org/
---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+requires = [
+    'sphinxcontrib-blockdiag', 'blockdiag>=1.5.0', 'Sphinx>=0.6', 'setuptools']
+
+setup(
+    name='sphinxcontrib-blockdiag',
+    version='0.1.0',
+    #    url='',
+    #    download_url='',
+    license='BSD',
+    author='Martin Drohmann',
+    author_email='mdrohmann@gmail.com',
+    description='Sphinx Project timeline extension',
+    long_description=open("README.rst").read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+)
